e9e8dfebaae617cd0ff2528b8a8acc027fdb36de
diegoxd93 <bolivaristadiego@gmail.com>
2022-01-08 12:35:24 -0400

    reafactor: card link redirection

--- src/components/CountryCard/index.jsx
+++ src/components/CountryCard/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './index.css'
 
 const CountryCard = ({
@@ -6,8 +7,9 @@ const CountryCard = ({
   region = "SIN ESPECIFICAR",
   capital = "NO SE CONOCE",
   flags = 'https://via.placeholder.com/500?text=BANDERA',
+  alpha3Code,
 }) => (
-  <div className='card'>
+  <Link to={`/${alpha3Code}`} className='card'>
     <img className='card-image' src={flags.png} alt={name} />
     <div className='card-content'>
       <h3 className='card-title'>{name}</h3>
@@ -15,7 +17,7 @@ const CountryCard = ({
       <p className='card-text'><span>Region: </span> {region}</p>
       <p className='card-text'><span>Capital: </span> {capital}</p>
     </div>
-  </div>
+  </Link>
 )
 
 export default CountryCard
